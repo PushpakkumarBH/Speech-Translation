097bab47154c04dac229308792e2373a8259f470
Pushpakkumar <57779932+PushpakkumarBH@users.noreply.github.com>
2022-11-09 23:24:24 +0530

    added flask app config

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template,request
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = '/home/username/uploads'
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file1():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return '<h1>file uploaded successfully</h1>'
+
+if __name__ == "__main__":
+   app.run()

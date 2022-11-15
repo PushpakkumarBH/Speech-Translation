8b279ebe911b0d72540dedc384ee8598be52728c
DurgaShree010404 <durgamjshree14@gmail.com>
2022-11-15 16:03:18 +0530

    Updated Video to Audio File

--- templates/videotoaudio.py
+++ templates/videotoaudio.py
@@ -1,12 +1,22 @@
 import moviepy.editor as mp
+from pydub import AudioSegment
+def video_to_audio():
+    my_clip = mp.VideoFileClip(r"videotest.mov")
+    my_clip.audio.write_audiofile(r"my_result.mp3")
 
-my_clip = mp.VideoFileClip(r"videotest.mov")
-my_clip.audio.write_audiofile(r"my_result.mp3")
+video_to_audio()
 
-# import required modules
-import subprocess
 
-# convert mp3 to wav file
-subprocess.call(['ffmpeg', '-i', 'my_result.mp3',
-				'converted_to_wav_file.wav'])
 
+
+def mp3_to_wav():
+
+  # files                                                                         
+  src = "E:\finalproject\my_result.mp3"
+  dst = "testf.wav"
+
+  # convert wav to mp3                                                            
+  sound = AudioSegment.from_mp3(r"E:\finalproject\my_result.mp3")
+  sound.export(dst, format="wav")
+
+mp3_to_wav()
